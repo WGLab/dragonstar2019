bc53604a10b72db6131c493866d2448cc4c229b4
Kai Wang <kaichop@gmail.com>
2019-07-24 22:16:11 -0400

    delete test_scripts

--- test_scripts/README.md
+++
@@ -1,9 +0,0 @@
-Run the following command one by one by test the server operation
-
-```
-bash /shared/EXERCISE/test_scripts/test_phenotype.sh
-bash /shared/EXERCISE/test_scripts/test_cnv.sh
-bash /shared/EXERCISE/test_scripts/test_annotation.sh
-bash /shared/EXERCISE/test_scripts/test_alignment.sh
-bash /shared/EXERCISE/test_scripts/test_assembly.sh
-```
--- test_scripts/test_phenotype.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/project/phenotype
-cd ~/project/phenotype
-disease_annotation.pl alzheimer -prediction -phenotype -logistic -out ex1
-disease_annotation.pl "Amyotrophic lateral sclerosis" -prediction -phenotype -logistic -out ex2
-disease_annotation.pl /shared/tools/phenolyzer/example_phenotype.txt -file -prediction -phenotype -logistic -out ex3
-disease_annotation.pl /shared/tools/phenolyzer/example_disease.txt -file -prediction -logistic -out ex4
-disease_annotation.pl /shared/tools/phenolyzer/example_hpo.txt -file -prediction -phenotype -logistic -out ex5
-cd ~/project
